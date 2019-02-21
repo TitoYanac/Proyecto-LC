eb76722afd25d5570003530b9c0191b0f558333a
Tito <titoyanac@gmail.com>
2019-02-21 02:08:02 -0500

    Add files via upload

--- js/animacion.js
+++ js/animacion.js
@@ -53,3 +53,23 @@ op3.addEventListener("click", function(event) {
 	slide2.setAttribute("style", "opacity: 1; display: none;");
 	slide3.setAttribute("style", "opacity: 1; display: block;");
 });
+
+$('#borrar-boton').click(function () { borrarCodigo(); });
+
+	function borrarCodigo() {
+
+		var codigoArea_text = $('#codigo');
+
+		var codigoArea = codigoArea_text.val();
+
+		if (codigoArea == '') {
+			alert("No hay nada que borrar");
+			codigoArea_text.focus();
+		} else {
+			codigoArea_text.val('');
+			document.getElementById("codeLine").innerHTML = '';
+			var pl = document.createElement('p');
+			pl.innerHTML = 1;
+			document.getElementById("codeLine").appendChild(pl);
+		}
+	}
