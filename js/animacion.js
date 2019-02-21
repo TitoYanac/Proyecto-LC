abe870327811bffd66520feeef829907f7822ec5
Tito <titoyanac@gmail.com>
2019-02-21 01:59:09 -0500

    Add files via upload

---
+++ js/animacion.js
@@ -0,0 +1,55 @@
+var TextAreaCodigo = document.getElementById("codigo");
+var LineasDeCodigo = document.getElementById("codeLine");
+
+TextAreaCodigo.onscroll = function() {
+	var x = TextAreaCodigo.scrollTop;
+	LineasDeCodigo.firstChild.setAttribute("style", "margin-top: -"+x+"px");
+};
+
+TextAreaCodigo.addEventListener("keyup", function(event) {
+	LineasDeCodigo.innerHTML ='';
+	var ArraydeLineas = TextAreaCodigo.value.split("\n");
+	var SaltosDeLinea = ArraydeLineas.length;
+	for (var i = 0; i < SaltosDeLinea; i++) {
+		var linea=document.createElement('p');
+		linea.innerHTML =i+1;
+		LineasDeCodigo.appendChild(linea);
+	}
+	var x = TextAreaCodigo.scrollTop;
+	LineasDeCodigo.firstChild.setAttribute("style", "margin-top: -"+x+"px");
+});
+
+var menu = document.getElementById("overMenu");
+var op1 = document.getElementById("t1");
+var op2 = document.getElementById("t2");
+var op3 = document.getElementById("t3");
+var DisplayMenu=0;
+menu.addEventListener("click", function(event) {
+	if(DisplayMenu==0){
+		op1.setAttribute("style", "opacity: 1; display: block;");
+		op2.setAttribute("style", "opacity: 1; display: block;");
+		op3.setAttribute("style", "opacity: 1; display: block;");
+		DisplayMenu=1;
+	}else{
+		op1.setAttribute("style", "opacity: 0; display: none;");
+		op2.setAttribute("style", "opacity: 0; display: none;");
+		op3.setAttribute("style", "opacity: 0; display: none;");
+		DisplayMenu=0;
+	}
+});
+
+op1.addEventListener("click", function(event) {
+	slide1.setAttribute("style", "opacity: 1; display: block;");
+	slide2.setAttribute("style", "opacity: 1; display: none;");
+	slide3.setAttribute("style", "opacity: 1; display: none;");
+});
+op2.addEventListener("click", function(event) {
+	slide1.setAttribute("style", "opacity: 1; display: none;");
+	slide2.setAttribute("style", "opacity: 1; display: block;");
+	slide3.setAttribute("style", "opacity: 1; display: none;");
+});
+op3.addEventListener("click", function(event) {
+	slide1.setAttribute("style", "opacity: 1; display: none;");
+	slide2.setAttribute("style", "opacity: 1; display: none;");
+	slide3.setAttribute("style", "opacity: 1; display: block;");
+});
